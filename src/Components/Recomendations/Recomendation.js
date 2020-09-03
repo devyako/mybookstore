b3f771ef1d24bd0f1850b3a67c0538f55c557c29
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-03 23:29:09 +0500

    update

--- src/Components/Recomendations/Recomendation.js
+++ src/Components/Recomendations/Recomendation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const recommendation = props => {
+const recommendation = (props) => {
   return (
     <Container>
       <div className="txt-img">
@@ -29,33 +29,39 @@ const Container = styled.div`
     width: 125px;
     margin-bottom: 10px;
     box-sizing: border-box;
-    -webkit-box-shadow: 10px 10px 10px 0px rgba(110, 102, 110, 0.52);
-    -moz-box-shadow: 10px 10px 10px 0px rgba(110, 102, 110, 0.52);
-    box-shadow: 10px 10px 10px 0px rgba(110, 102, 110, 0.52);
-
+    box-shadow: 0px 0px 10px 5px rgba(112, 112, 112, 0.25);
+    
     transition: 0.4s;
     :hover {
       transition: 0.3s;
       cursor: pointer;
       transform: scale(1.2);
+      border-radius: 5px;
     }
   }
 
   .buttons {
+    width: 10rem;
+    height: 2.5rem;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+
     button {
       font-family: "Raleway", sans-serif;
       cursor: pointer;
       background: linear-gradient(90deg, #ef9d23 -0.77%, #f05959 97.68%);
       border-radius: 4px;
       border: transparent;
-      padding: 5px;
+      padding: 10px;
       color: white;
-      margin-left: 5px;
+      transition: 0.5s ease;
+      box-sizing: border-box;
 
       :hover {
-        background: linear-gradient(90deg, #f05959 -0.77%, #ef9d23 97.68%);
+        transition: 0.5s ease;
+        padding: 5px;
+        box-shadow: 10px 10px 10px 2px rgba(239, 157, 35, 0.42);
       }
     }
     .amazon {
@@ -63,12 +69,13 @@ const Container = styled.div`
       background: linear-gradient(90deg, #f05959 -0.77%, #ef9d23 97.68%);
       border-radius: 4px;
       border: transparent;
-      padding: 5px;
+      padding: 10px;
       color: white;
-      margin-left: 5px;
+      transition: 0.5s ease;
 
       :hover {
-        background: linear-gradient(90deg, #ef9d23 -0.77%, #f05959 97.68%);
+        transition: 0.5s ease;
+        box-shadow: 10px 10px 10px 2px rgba(239, 157, 35, 0.42);
       }
     }
   }
